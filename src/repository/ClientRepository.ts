5f87d4799c65fd097671df8a0ddc4c4d8ef7b384
devpabloh <devpabloh@gmail.com>
2025-10-25 17:19:25 -0300

    ✨ feat: criando os repositorys com os métodos necessários para a API

--- src/repository/ClientRepository.ts
+++ src/repository/ClientRepository.ts
@@ -1,7 +1,42 @@
-import { PrismaClient } from "@prisma/client";
-import { Client } from "../models/Client.js";
+import {PrismaClient} from '@prisma/client'
+import type { Client } from "../models/Client.js";
 import {prisma} from '../lib/prisma.js'
 
 export class clientRepository{
-    
+    private prisma: PrismaClient
+
+    constructor(){
+      this.prisma = prisma
+    }
+
+    public async create(data: Omit<Client,'id'>): Promise<Client>{
+      return this.prisma.client.create({data})
+    }
+
+    public async findAll(): Promise<Client[]>{
+      return this.prisma.client.findMany()
+    }
+
+    public async findById(id:number): Promise<Client | null>{
+      return this.prisma.client.findUnique({where: {id}})
+    }
+
+    public async findByName(name: string): Promise<Client[]>{
+      return this.prisma.client.findMany({where: {name: {contains: name}}})
+    }
+
+    public async count(): Promise<number>{
+      return this.prisma.client.count()
+    }
+
+    public async update(id: number, data: Partial<Omit<Client,'id'>>):Promise<Client | null>{
+      return this.prisma.client.update({
+        where: {id},
+        data,
+      })
+    }
+
+    public async delete(id:number): Promise<void>{
+      await this.prisma.client.delete({where: {id}})
+    }
 }
