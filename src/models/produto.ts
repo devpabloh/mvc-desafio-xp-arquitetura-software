eaf71808b35db2576694b3844a9d75befd478100
pablohenriqueadm94@gmail.com <pablohenriqueadm94@gmail.com>
2025-10-25 15:26:16 -0300

    :sparkles: feat: criando a entidade do produto na pasta de models

---
+++ src/models/produto.ts
@@ -0,0 +1,9 @@
+    export interface Produto{
+        id: string;
+        name: string;
+        price: string;
+        stock: string;
+        description: string;
+        createdAt: Date;
+        updatedAt: Date;
+    }
